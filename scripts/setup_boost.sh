832796f0131f2ea71044a727190f147cedc855e5
Lorenzo <887857@stud.unive.it>
2023-12-31 11:18:50 +0100

    Change paths

--- scripts/setup_boost.sh
+++ scripts/setup_boost.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-BOOST_SRC_DIR="$PWD"/vendor/boost
-BOOST_INSTALL_DIR="$PWD"/libs
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+BOOST_SRC_DIR="$SCRIPT_DIR"/../boost
+BOOST_INSTALL_DIR="$SCRIPT_DIR"/../libs
 
 BI_SRC_DIR="$PWD"
 
