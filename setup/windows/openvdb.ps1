35e9a5ef38bf30980cda62a274721276d8270124
Lorenzo Fanton <887857@stud.unive.it>
2024-01-03 14:42:46 +0100

    Cannot build on Windows

--- setup/windows/openvdb.ps1
+++ setup/windows/openvdb.ps1
@@ -11,12 +11,13 @@ cmake                                             `
     -DCMAKE_INSTALL_PREFIX="$OPENVDB_INSTALL_DIR" `
     -DOPENVDB_BUILD_CORE=ON                       `
     -DOPENVDB_INSTALL_CMAKE_MODULES=ON            `
-    -DOPENVDB_CORE_STATIC=ON                      `
+    -DOPENVDB_CORE_STATIC=OFF                      `
     -DOPENVDB_CORE_SHARED=ON                      `
     -DOPENVDB_BUILD_BINARIES=OFF                  `
     -DUSE_STATIC_DEPENDENCIES=OFF                 `
     -DTBB_ROOT="$OPENVDB_INSTALL_DIR"             `
-    -DTBB_USE_STATIC_LIBS=OFF                     `
+    -DTBB_LIBRARYDIR="$OPENVDB_INSTALL_DIR/lib"             `
+    -DTBB_USE_STATIC_LIBS=ON                     `
     -DBoost_ROOT="$OPENVDB_INSTALL_DIR"           `
     -DBoost_USE_STATIC_LIBS=ON                    `
     -DUSE_BLOSC=ON                                `
@@ -28,5 +29,5 @@ cmake                                             `
     -S "$OPENVDB_SRC_DIR"                         `
     --fresh
 
-cmake --build "$OPENVDB_BUILD_DIR" --config Release --parallel 6
-cmake --install "$OPENVDB_BUILD_DIR"
+cmake --build "$OPENVDB_BUILD_DIR" --config Debug --parallel 6
+cmake --install "$OPENVDB_BUILD_DIR" --config Debug
