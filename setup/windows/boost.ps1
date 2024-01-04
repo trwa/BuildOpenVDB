5dc42439812d31193fd6e013715a426a3e4bf417
Lore <lorenzo.fanton@outlook.it>
2024-01-04 15:55:20 +0100

    Boost on Windows (untested)
    https://www.boost.org/doc/libs/1_84_0/more/getting_started/windows.html#or-build-from-the-command-prompt

--- setup/windows/boost.ps1
+++ setup/windows/boost.ps1
@@ -1,11 +1,11 @@
 $ROOT_DIR = Join-Path "$PSScriptRoot" "/../.."
 
 $BOOST_SRC_DIR = Join-Path "$ROOT_DIR" "/boost"
-$BOOST_INSTALL_DIR = Join-Path "$ROOT_DIR" "/libs"
+$BOOST_INSTALL_DIR = Join-Path "$ROOT_DIR" "/install"
 
-Set-Location "$BOOST_SRC_DIR"
+Set-Location (Join-Path "$BOOST_SRC_DIR" "/tools/build")
 
 ./bootstrap.bat
-./b2 variant=release link=static threading=multi install --prefix="$BOOST_INSTALL_DIR" --with-iostreams --with-system
+./b2 install --prefix="$BOOST_INSTALL_DIR" variant=release link=static threading=multi --with-iostreams
 
 Set-Location "$ROOT_DIR"
